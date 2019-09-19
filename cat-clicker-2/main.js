60c857208ca2c56f04705fb6ab343fada815d84f
Amir Makram <amakram97@gmail.com>
2019-09-19 16:25:22 +0200

    adding another version in catClicker using knockoutjs

---
+++ cat-clicker-2/main.js
@@ -0,0 +1,21 @@
+var ViewModel = function(){
+    this.clickCounter = ko.observable(0);
+    this.name = ko.observable('Haha');
+    this.image = ko.observable('cat1.jpg');
+    this.imageClicked = function(){
+        let oldCounter = this.clickCounter();
+        this.clickCounter(oldCounter+1);
+    };
+    this.level = ko.observable('new born');
+    this.levelChange = ko.computed(function(){
+        let level = 'newborn';
+        let checkcounter = this.clickCounter();
+        if(checkcounter>10){
+            level = 'teen';
+        }
+        return level;
+    },this);
+    this.nickNames = ko.observableArray([]);
+}
+
+ko.applyBindings(new ViewModel());
